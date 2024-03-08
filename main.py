80ecb690d0ceb229088eee233e359a057407cde0
Sedo-KFM <fedor122448@gmail.com>
2024-03-09 01:42:02 +0400

    Compress en2ru dictionary initialization

--- main.py
+++ main.py
@@ -17,76 +17,10 @@ insertion_any_form = NewType('insertion_any_form', Union[
 
 insertion = NewType('insertion', list[tuple[str, int]])
 
-en2ru = {
-    '`': 'ё',
-    'q': 'й',
-    'w': 'ц',
-    'e': 'у',
-    'r': 'к',
-    't': 'е',
-    'y': 'н',
-    'u': 'г',
-    'i': 'ш',
-    'o': 'щ',
-    'p': 'з',
-    '[': 'х',
-    ']': 'ъ',
-    'a': 'ф',
-    's': 'ы',
-    'd': 'в',
-    'f': 'а',
-    'g': 'п',
-    'h': 'р',
-    'j': 'о',
-    'k': 'л',
-    'l': 'д',
-    ';': 'ж',
-    "'": 'э',
-    'z': 'я',
-    'x': 'ч',
-    'c': 'с',
-    'v': 'м',
-    'b': 'и',
-    'n': 'т',
-    'm': 'ь',
-    ',': 'б',
-    '.': 'ю',
-    '~': 'Ё',
-    'Q': 'Й',
-    'W': 'Ц',
-    'E': 'У',
-    'R': 'К',
-    'T': 'Е',
-    'Y': 'Н',
-    'U': 'Г',
-    'I': 'Ш',
-    'O': 'Щ',
-    'P': 'З',
-    '{': 'Х',
-    '}': 'Ъ',
-    'A': 'Ф',
-    'S': 'Ы',
-    'D': 'В',
-    'F': 'А',
-    'G': 'П',
-    'H': 'Р',
-    'J': 'О',
-    'K': 'Л',
-    'L': 'Д',
-    ':': 'Ж',
-    '"': 'Э',
-    'Z': 'Я',
-    'X': 'Ч',
-    'C': 'С',
-    'V': 'М',
-    'B': 'И',
-    'N': 'Т',
-    'M': 'Ь',
-    '<': 'Б',
-    '>': 'Ю'
-}
-
-ru2en = {v: k for k, v in en2ru.items()}
+en_keyboard = '`qwertyuiop[]asdfghjkl;\'zxcvbnm,./~QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?'
+ru_keyboard = 'ёйцукенгшщзхъфывапролджэячсмитьбю.ЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,'
+en2ru = {e: r for e, r in zip(en_keyboard, ru_keyboard)}
+ru2en = {r: e for e, r in zip(en_keyboard, ru_keyboard)}
 
 
 def visible_len(line: str) -> int:
